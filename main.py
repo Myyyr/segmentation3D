08e486939ed2c57c053e1deae2831949dddfec24
MYR <loic.themyr@gmail.com>
2020-11-05 15:30:02 +0100

    add main

---
+++ main.py
@@ -0,0 +1,29 @@
+import alltrain.BratsTrain as BratsTrain
+import  argparse
+
+
+
+
+
+def main(config):
+	if config == 'example':
+		import expconfigs.example as cfg
+
+
+		excfg = cfg.ExpConfig()
+
+		train = BratsTrain.BratsTrain(excfg)
+
+		train.train()
+
+
+
+
+
+if __name__ == "__main__" :
+	parser = argparse.ArgumentParser("Configs for training")
+	parser.add_argument('-c', '--config', help='Config to use', required=True)
+
+	main(parser.config)
+
+
