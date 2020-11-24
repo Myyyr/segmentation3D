e0d9a02a9bce0cfe9e6b3496586d0f8fe5b5fff0
Loïc THEMYR <themyr@pop-os.localdomain>
2020-11-24 14:44:54 +0100

    debug loss

--- multiatlasDataset.py
+++ multiatlasDataset.py
@@ -40,7 +40,7 @@ class MultiAtlasDataset(torch.utils.data.Dataset):
 
         #lazily open file
         self.openFileIfNotOpen()
-        print(self.labelpath)
+        print(self.labelPath)
         print(self.filePath)
         print("## LEN", self.__len__())
         print("hdf5 keys", self.file.keys())
