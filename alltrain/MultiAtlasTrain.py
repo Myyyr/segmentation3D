d74134b4cf8cfca8e234bc72339f5926c94fc368
Loïc THEMYR <themyr@pop-os.localdomain>
2020-11-27 15:58:19 +0100

    memory

--- alltrain/MultiAtlasTrain.py
+++ alltrain/MultiAtlasTrain.py
@@ -93,7 +93,6 @@ class MemMATrain(Train):
                 self.back_step(expcf, loss)
                 del loss
 
-                break
 
             print("epoch: {}, total_loss: {}, mem: {}".format(epoch, total_loss/int(len(self.trainDataLoader)), str(self.convert_byte(torch.cuda.max_memory_allocated())) ) )
 
