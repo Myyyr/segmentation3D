abd5913becd6ad9e0677b1c53405d31bd49800dc
Loïc THEMYR <themyr@pop-os.localdomain>
2020-11-30 16:39:15 +0100

    configs

--- expconfigs/multi_atlas_unet.py
+++ expconfigs/multi_atlas_unet.py
@@ -23,7 +23,7 @@ class ExpConfig():
         # Model
         self.channels = [64, 128, 256, 512, 1024]
         self.channels = [int(x/8) for x in self.channels]
-        self.net = RevUnet3D(1, self.channels, 12, interpolation = (256,256,99))
+        self.net = RevUnet3D(1, self.channels, 12, interpolation = (512,512,198))
 
         # Data
         self.nn_augmentation = False
