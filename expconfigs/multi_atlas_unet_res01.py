022ecf40374125cab4fe6956fc3f89a34f2d3ec5
Loïc THEMYR <themyr@pop-os.localdomain>
2020-12-01 16:25:05 +0100

    debug

--- expconfigs/multi_atlas_unet_res01.py
+++ expconfigs/multi_atlas_unet_res01.py
@@ -50,7 +50,12 @@ class ExpConfig():
         #                       momentum=0.9,
         #                       nesterov=True,
         #                       weight_decay=1e-5) #todo
-        self.optimizer = optim.Adam(self.net.parameters(), lr = 5e-4, weight_decay=1e-5)
+        # self.optimizer = optim.Adam(self.net.parameters(), lr = 5e-4, weight_decay=1e-5)
+        self.optimizer = optim.SGD(self.net.parameters(),
+                                    lr=self.lr_rate,
+                                    momentum=0.9,
+                                    nesterov=True,
+                                    weight_decay=self.l2_reg_weight)
         self.validate_every_k_epochs = 1
         # Scheduler list : [lambdarule_1]
         self.lr_scheduler = get_scheduler(self.optimizer, "multistep")
