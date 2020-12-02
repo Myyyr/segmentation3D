8e70c03901394dc68db4e7ed128fbcfe23d0ab34
MYR <loic.themyr@gmail.com>
2020-12-02 15:48:55 +0100

    look data

--- look_main.py
+++ look_main.py
@@ -23,6 +23,8 @@ def main(config):
 if __name__ == "__main__" :
 	parser = argparse.ArgumentParser("Configs for training")
 	parser.add_argument('-c', '--config', help='Config to use', required=True)
+	parser.add_argument('-s', '--start', help='start image', required=True)
+	parser.add_argument('-e', '--end', help='last image', required=True)
 
 	main(parser.parse_args().config)
 
