f3b37ce27077eeb3664563bfaee05db99d29a26b
Loïc THEMYR <themyr@pop-os.localdomain>
2020-12-03 11:36:08 +0100

    look images

--- look_main.py
+++ look_main.py
@@ -5,7 +5,7 @@ import  argparse
 
 
 
-def main(config, s, e):
+def main(config, s, e, mod):
 	if config == 'mat':
 		import expconfigs.multi_atlas_unet as cfg
 
@@ -14,8 +14,10 @@ def main(config, s, e):
 
 		train = data_process.LookMAT(excfg)
 
-		train.train(s, e)
-
+		if mod == 'look':
+			train.train(s, e)
+		elif mod == 'info':
+			train.data_info()
 		return 0
 
 	
@@ -25,7 +27,8 @@ if __name__ == "__main__" :
 	parser.add_argument('-c', '--config', help='Config to use', required=True)
 	parser.add_argument('-s', '--start', help='start image', required=True)
 	parser.add_argument('-e', '--end', help='last image', required=True)
+	parser.add_argument('-m', '--mode', help='data look mode', required=True)
 
-	main(parser.parse_args().config, int(parser.parse_args().start), int(parser.parse_args().end))
+	main(parser.parse_args().config, int(parser.parse_args().start), int(parser.parse_args().end), parser.parse_args().mode)
 
 
