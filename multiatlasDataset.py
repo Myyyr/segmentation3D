d5165e17af7843aea7303b22189879deb727ae7e
Loïc THEMYR <themyr@pop-os.localdomain>
2020-12-03 16:54:09 +0100

    look images

--- multiatlasDataset.py
+++ multiatlasDataset.py
@@ -52,6 +52,7 @@ class MultiAtlasDataset(torch.utils.data.Dataset):
         if self.hasMasks: 
             labels = self.labelFile["masks_" + 'train'][index, ...]
             print( 'np.sum(labels)' ,np.sum(labels))
+            print( 'np.prod(labels.shape)' ,np.prod(labels.shape))
             exit(0)
         if self.look_small: 
             smalllabels = self.file["masks_" + 'train'][index, ...]
