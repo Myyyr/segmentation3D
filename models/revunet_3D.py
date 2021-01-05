e05bf0628220e2435f01503838186a81131a4cae
MYR <loic.themyr@gmail.com>
2021-01-05 11:56:58 +0100

    debug

--- models/revunet_3D.py
+++ models/revunet_3D.py
@@ -94,9 +94,9 @@ class DecoderModule(nn.Module):
         if self.upsample:
             x = self.conv(x)
             # x = F.interpolate(x, scale_factor=2, mode="trilinear", align_corners=False)
-            print('x s ', x.shape)
-            print('i s ', shape)
-            x = F.interpolate(x, size=shape)
+            # print('x s ', x.shape)
+            # print('i s ', shape)
+            x = F.interpolate(x, size=shape[-3:])
         # for i in range(1,4):
         #     # print("#" ,x.shape,shape)
         #     if x.shape[-i] != shape[-i]:
