419c1d5059b21c7be9145d705376304570a1cfc9
MYR <loic.themyr@gmail.com>
2021-01-08 15:45:43 +0100

    debug

--- expconfigs/multi_atlas_revunet_016.py
+++ expconfigs/multi_atlas_revunet_016.py
@@ -16,7 +16,7 @@ class ExpConfig():
         # ID and Name
         self.experiment_name = "atlas_revunet_3D_016"
         self.id = 16
-        self.debug = True
+        self.debug = False
 
         # System
         self.checkpointsBasePath = "./checkpoints/"
