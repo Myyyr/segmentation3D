0ba9203a27dd4c30552b489705bd6dab43f73655
MYR <loic.themyr@gmail.com>
2021-01-21 15:58:40 +0100

    try other scheduler and smaller net - debug

--- models/utils.py
+++ models/utils.py
@@ -11,7 +11,7 @@ def get_scheduler(optimizer, opt, lr):
             #print(epoch)
             if epoch < 60:
                 lr_l = 0.01
-            elif 60 <= epoch < 101:
+            else:
                 lr_l = 0.002
             return lr_l
         scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda_rule) 
