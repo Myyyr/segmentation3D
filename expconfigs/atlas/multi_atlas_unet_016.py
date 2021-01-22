8f18f59c1038c6b82cb7f9424893c678eccd927f
MYR <loic.themyr@gmail.com>
2021-01-22 08:26:35 +0100

    debug

--- expconfigs/atlas/multi_atlas_unet_016.py
+++ expconfigs/atlas/multi_atlas_unet_016.py
@@ -29,7 +29,7 @@ class ExpConfig():
         self.datapath = "/local/SSD_DEEPLEARNING/MULTI_ATLAS/multi_atlas/data_3D_size_80_80_32_res_0.16.hdf5"
         
         # GPU
-        self.gpu = '0'
+        self.gpu = '1'
         os.environ["CUDA_VISIBLE_DEVICES"] = self.gpu
 
         # Model
