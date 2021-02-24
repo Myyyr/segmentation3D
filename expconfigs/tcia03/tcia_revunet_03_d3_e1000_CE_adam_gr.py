1c85472ebbbce2d397b23fd1f8474d02bf15bbd2
MYR <loic.themyr@gmail.com>
2021-02-24 09:53:17 +0100

    tcia no grnorm

--- expconfigs/tcia03/tcia_revunet_03_d3_e1000_CE_adam_gr.py
+++ expconfigs/tcia03/tcia_revunet_03_d3_e1000_CE_adam_gr.py
@@ -44,7 +44,7 @@ class ExpConfig():
         self.n_parameters = count_parameters(self.net)
         print("N PARAMS : {}".format(self.n_parameters))
 
-        self.model_path = './checkpoints/models/revunet_tcia_160_160_64_d3.pth'
+        self.model_path = './checkpoints/models/revunet_tcia_160_160_64_d3_gr1.pth'
         self.load_model()
         self.split = 1
          
