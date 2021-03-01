e131cc1b7ece683c4f5426c7bd93d8efb176f703
MYR <loic.themyr@gmail.com>
2021-03-01 11:49:07 +0100

    rev atlas big - degug memory

--- expconfigs/atlas1/multi_atlas_revunet_d3_e2000_CE_adam_lr.py
+++ expconfigs/atlas1/multi_atlas_revunet_d3_e2000_CE_adam_lr.py
@@ -34,14 +34,14 @@ class ExpConfig():
 
         # Model
         # self.channels = [64, 128, 256, 512, 1024]
-        self.channels = [2, 2, 2, 2, 2]
+        self.channels = [2, 2, 2, 2]
         self.channels = [int(x//1) for x in self.channels]
         self.net = RevUnet3D(1, self.channels, 14, depth = 1 ,interpolation = None, groups=1)#(512,512,198))
         # self.net = RevUnet3D(1, self.channels, 12, interpolation = (256,256,99))
         self.n_parameters = count_parameters(self.net)
         print("N PARAMS : {}".format(self.n_parameters))
 
-        self.model_path = './checkpoints/models/revunet_atlas_512_512_208_d3_fp_gr1.pth'
+        self.model_path = './checkpoints/models/revunet_atlas_512_512_208_d3_fp2_gr1.pth'
         self.load_model()
 
         self.n_classes = 14 
