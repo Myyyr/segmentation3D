b6952c3a452e046876274c6fc286b19faf2c014a
MYR <loic.themyr@gmail.com>
2021-03-05 12:19:42 +0100

    iunet - debug

--- expconfigs/atlas1/multi_atlas_iunet_e2000_CE_adam_lr.py
+++ expconfigs/atlas1/multi_atlas_iunet_e2000_CE_adam_lr.py
@@ -28,7 +28,7 @@ class ExpConfig():
         self.datapath = self.labelpath
         
         # GPU
-        self.gpu = '0'
+        self.gpu = '1'
         os.environ["CUDA_VISIBLE_DEVICES"] = self.gpu
 
         # Model
