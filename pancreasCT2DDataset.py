ffb08ba21c10d144ea0397cd3bb90c37327aa565
MYR <loic.themyr@gmail.com>
2021-03-23 15:40:36 +0100

    utrans 2d - debug

--- pancreasCT2DDataset.py
+++ pancreasCT2DDataset.py
@@ -28,7 +28,7 @@ class SplitTCIA2DDataset(data.Dataset):
 
         for i in data_splits:
             for j in listdir(join(root_dir, 'images')):
-                # print(i, j)
+                print(i, j)
                 # print(j)
                 image_dir = join(root_dir, 'images', str(i), str(j))
                 target_dir = join(root_dir, 'annotations', str(i), str(j))
