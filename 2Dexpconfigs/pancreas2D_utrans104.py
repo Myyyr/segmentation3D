1e65ff0119165f241839227966e9315bd8133bb3
MYR <loic.themyr@gmail.com>
2021-04-08 14:56:19 +0200

    config

--- 2Dexpconfigs/pancreas2D_utrans104.py
+++ 2Dexpconfigs/pancreas2D_utrans104.py
@@ -30,7 +30,7 @@ class ExpConfig():
         self.split = 0
         
         # GPU
-        self.gpu = '0'
+        self.gpu = '1'
         os.environ["CUDA_VISIBLE_DEVICES"] = self.gpu
 
         # Model
