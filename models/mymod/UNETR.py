6b5e7c4e307e1399489a6fd5c80a88202bae20fb
Loïc THEMYR <themyr@pop-os.localdomain>
2021-04-13 16:38:39 +0200

    all

--- models/mymod/UNETR.py
+++ models/mymod/UNETR.py
@@ -78,12 +78,7 @@ class UNETR(nn.Module):
 
         # Decode
         X = self.up_concat4(sk123[3])
-        print('sk123[2].shape', sk123[2].shape)
-        b = self.skip3(sk123[2])
-        print(shape(b))
-        a = torch.cat([b, X],1)
-        print('a.shape', a.shape)
-        X = self.up_concat3(a)
+        X = self.up_concat3(torch.cat([self.skip3(sk123[2]), X],1))
         X = self.up_concat2(torch.cat([self.skip2(sk123[1]), X],1))
         X = self.up_concat1(torch.cat([self.skip1(sk123[0]), X],1))
 
