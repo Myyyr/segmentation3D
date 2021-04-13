4aef98eee109e6aae26b98e72e67703acf4acd7c
Loïc THEMYR <themyr@pop-os.localdomain>
2021-04-13 15:55:57 +0200

    all

--- models/mymod/UNETR.py
+++ models/mymod/UNETR.py
@@ -56,11 +56,12 @@ class UNETR(nn.Module):
 
 
     def forward(self, X, mode=None):
-
+        bs = X.shape[0]
 
         sk0 = self.skip0(X)
         sk123 = []
-
+        self.emb_size_reshape = [bs] + self.emb_size_reshape
+        self.emb_size_flat = [bs] + self.emb_size_flat
         # Get patches, flat and project
         X = torch.reshape(X, self.emb_size_reshape)
         print('X.shape', X.shape)
