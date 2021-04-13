4fee8b22b1c48dbdca0f715594270185c8229faa
Loïc THEMYR <themyr@pop-os.localdomain>
2021-04-13 15:51:48 +0200

    all

--- models/mymod/UNETR.py
+++ models/mymod/UNETR.py
@@ -51,7 +51,7 @@ class UNETR(nn.Module):
 
         # initialise weights
         for m in self.modules():
-            if isinstance(m, nn.ConvTranspose3d) or isinstance(m, nn.Conv3d), or isinstance(m, nn.TransformerEncoderLayer):
+            if isinstance(m, nn.ConvTranspose3d) or isinstance(m, nn.Conv3d) or isinstance(m, nn.TransformerEncoderLayer):
                 init_weights(m, init_type='kaiming')
 
 
