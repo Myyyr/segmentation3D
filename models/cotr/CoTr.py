6193680eb5dc0679610ff9210d50ecd1ea3d4879
MYR <loic.themyr@gmail.com>
2021-04-28 16:28:39 +0200

    cotr debug

--- models/cotr/CoTr.py
+++ models/cotr/CoTr.py
@@ -156,10 +156,10 @@ class U_ResTran3D(nn.Module):
         print(inputs.shape)
         print(x_convs[-1].shape)
         print(x_trans.shape)
-        print(x_trans[:, 512::].shape)
-        print(x_trans[:, 512::].transpose(-1, -2).shape)
-        x = self.transposeconv_stage2(x_trans[:, 512::].transpose(-1, -2).view(x_convs[-1].shape)) # x_trans length: 12*24*24+6*12*12=7776
-        skip2 = x_trans[:, 0:512].transpose(-1, -2).view(x_convs[-2].shape)
+        print(x_trans[:, 6912::].shape)
+        print(x_trans[:, 6912::].transpose(-1, -2).shape)
+        x = self.transposeconv_stage2(x_trans[:, 6912::].transpose(-1, -2).view(x_convs[-1].shape)) # x_trans length: 12*24*24+6*12*12=7776
+        skip2 = x_trans[:, 0:6912].transpose(-1, -2).view(x_convs[-2].shape)
         exit(0)
         x = x + skip2
         x = self.stage2_de(x)
