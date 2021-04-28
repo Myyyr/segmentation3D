c2f24455b0b06cb394db95f93458a2046cccf3c6
MYR <loic.themyr@gmail.com>
2021-04-28 16:26:58 +0200

    cotr debug

--- models/cotr/CoTr.py
+++ models/cotr/CoTr.py
@@ -94,7 +94,7 @@ class U_ResTran3D(nn.Module):
 
         self.MODEL_NUM_CLASSES = num_classes
 
-        self.upsamplex2 = nn.Upsample(scale_factor=(2,2,2), mode='trilinear')
+        self.upsamplex2 = nn.Upsample(scale_factor=(1,2,2), mode='trilinear')
 
         self.transposeconv_stage2 = nn.ConvTranspose3d(384, 384, kernel_size=(2,2,2), stride=(2,2,2), bias=False)
         self.transposeconv_stage1 = nn.ConvTranspose3d(384, 192, kernel_size=(2,2,2), stride=(2,2,2), bias=False)
@@ -160,7 +160,7 @@ class U_ResTran3D(nn.Module):
         print(x_trans[:, 512::].transpose(-1, -2).shape)
         x = self.transposeconv_stage2(x_trans[:, 512::].transpose(-1, -2).view(x_convs[-1].shape)) # x_trans length: 12*24*24+6*12*12=7776
         skip2 = x_trans[:, 0:512].transpose(-1, -2).view(x_convs[-2].shape)
-
+        exit(0)
         x = x + skip2
         x = self.stage2_de(x)
         ds2 = self.ds2_cls_conv(x)
