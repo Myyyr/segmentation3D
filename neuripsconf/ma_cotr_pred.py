c179314aaad429a4db61dcf04aeb6e311953ccb9
MYR <loic.themyr@gmail.com>
2021-04-29 22:27:41 +0200

    viz

--- neuripsconf/ma_cotr_pred.py
+++ neuripsconf/ma_cotr_pred.py
@@ -40,7 +40,7 @@ class ExpConfig():
         self.clip = True
         self.patched = True
         # GPU
-        self.gpu = '1'
+        self.gpu = '0'
         os.environ["CUDA_VISIBLE_DEVICES"] = self.gpu
 
         # Model
