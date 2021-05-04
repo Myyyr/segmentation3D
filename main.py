fccb96e8b7564d71940de7b5c5046b1baeb546c0
Loïc THEMYR <themyr@pop-os.localdomain>
2021-05-04 14:22:36 +0200

    all

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import importlib
 
 
 
-def main(config):
+def main(config, tensorboard):
 	
 	print(config)
 	cfg = importlib.import_module(config)
 	excfg = cfg.ExpConfig()
 
-	train = alltrain.AllTrain(excfg)
+	train = alltrain.AllTrain(excfg, tensorboard)
 	train.train()
 
 	return 0
@@ -20,7 +20,10 @@ def main(config):
 if __name__ == "__main__" :
 	parser = argparse.ArgumentParser("Configs for training")
 	parser.add_argument('-c', '--config', help='Config to use', required=True)
+	parser.add_argument('-t', "--tensorboard", type=str2bool, nargs='?',
+                        const=True, default=False,
+                        help="Activate tensorboard view.")
 
-	main(parser.parse_args().config)
+	main(parser.parse_args().config, parser.parse_args().tensorboard)
 
 
