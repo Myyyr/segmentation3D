047a5458855c62e40dd08c44e1407cc11321d879
MYR <loic.themyr@gmail.com>
2021-05-05 17:32:34 +0200

    add posenc - debug

--- models/mymod/cross_patch.py
+++ models/mymod/cross_patch.py
@@ -225,7 +225,7 @@ class CrossPatch3DTr(nn.Module):
 
         # Cross attention
         # print(A.shape)
-        print(Z.shape, rseq, R.shape)
+        print(A.shape, rseq, R.shape)
         Z = self.cross_trans(A, rseq)
         del A
         
