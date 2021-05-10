0b613274cac2af5815977a829b4204ee712077bd
MYR <loic.themyr@gmail.com>
2021-05-10 17:36:22 +0200

    cross model no cross - debug

--- neuripsconf/ma_cotr_v403.py
+++ neuripsconf/ma_cotr_v403.py
@@ -111,8 +111,8 @@ class ExpConfig():
         
     def set_data(self, split = 0):
         # Data
-        self.trainDataset = PatchedMultiAtlasDataset(self, mode="train", n_iter=250, patch_size=self.patch_size, return_full_image=False, ds_scales=self.ds_scales, do_tr=True)
-        self.testDataset  = PatchedMultiAtlasDataset(self, mode="test", n_iter=1, patch_size=self.patch_size, return_full_image=False, ds_scales=None, do_tr=False)
+        self.trainDataset = PatchedMultiAtlasDataset(self, mode="train", n_iter=250, patch_size=self.patch_size, return_full_image=False, ds_scales=self.ds_scales, do_tr=True, return_pos=False)
+        self.testDataset  = PatchedMultiAtlasDataset(self, mode="test", n_iter=1, patch_size=self.patch_size, return_full_image=False, ds_scales=None, do_tr=False, return_pos=False)
         self.trainDataLoader = DataLoader(dataset=self.trainDataset, num_workers=8, batch_size=self.batchsize, shuffle=True)
         self.testDataLoader = DataLoader(dataset=self.testDataset, num_workers=8, batch_size=1, shuffle=False)
 
