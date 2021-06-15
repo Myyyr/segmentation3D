ac0d2624d5e324ecfbe84cbd2388cb88dc499393
Loïc THEMYR <themyr@pop-os.localdomain>
2021-06-15 16:00:40 +0200

    cross - update to vit like transformers

--- models/mymod/transTools.py
+++ models/mymod/transTools.py
@@ -3,6 +3,114 @@ import torch.nn as nn
 import math
 from models.networks_other import init_weights
 
+from torch import nn, einsum
+import torch.nn.functional as F
+
+from einops import rearrange, repeat
+from einops.layers.torch import Rearrange
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout = 0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+    def forward(self, x):
+        return self.net(x)
+
+
+
+class MHCrossTransformer(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+        super(MHCrossTransformer).__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                PreNorm(dim, MHCrossAttention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
+                PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
+            ]))
+    def forward(self, x):
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+        return x
+
+class MHCrossAttention(nn.Module):
+    """docstring for MHCrossAttention"""
+
+    # !! Maybe add dropout later !!
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super(MHCrossAttention, self).__init__()
+        inner_dim = dim_head *  heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.attend = nn.Softmax(dim = -1)
+        # self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+        self.wq = nn.Linear(self.d_model, self.inner_dim, bias = False)
+        self.wk = nn.Linear(self.d_model, self.inner_dim, bias = False)
+        self.wv = nn.Linear(self.d_model, self.inner_dim, bias = False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+        #initialise the blocks
+        for m in self.children():
+            init_weights(m, init_type='kaiming')        
+
+
+    def forward(self, X, rseq=512):
+        # Normalization
+        # print('###X',X.shape)
+        X = self.norm1(X)
+        # print('###X',X.shape)
+
+        # Separation Region/FullImage into Xq / (Xk&v)
+        Xq, Xkv = X[:,:rseq,:], X[:,rseq:,:]
+        # print('###Xq',Xq.shape)
+        # print('###Xkv',Xkv.shape)
+
+        Z = []
+        # Compute attention for all heads
+        Q = self.wq(Xq)
+        K = self.wk(Xk)
+        V = self.wv(XV)
+
+        Q = rearrange(Q, 'b n (h d) -> b n h d',h=self.heads)
+        K = rearrange(K, 'b n (h d) -> b n h d',h=self.heads)
+        V = rearrange(V, 'b n (h d) -> b n h d',h=self.heads)
+
+        Z = self.attention(Q,K,V)
+        del Q, K, V
+        Z = rearrange(Z, 'b h n d -> b n (h d)')
+        Z = self.to_out(Z)
+
+        return Z
+
+
+
+    def attention(self, Q, K, V):
+        M = torch.matmul(Q,K.transpose(-2,-1))*self.scale
+        A = nn.functional.softmax(M, dim = -1)
+        del M
+        return torch.matmul(A,V)
 
 class CrossAttention(nn.Module):
     """docstring for CrossAttention"""
@@ -140,175 +248,7 @@ def positionalencoding2d(d_model, height, width):
 
     return pe
 
-class Trans2D(nn.Module):
-    def __init__(self, d, n_heads = 1):
-        super(Trans2D, self).__init__()
-
-        if d%n_heads != 0:
-            print("!!! d has to be a multiple of n_heads !!!")
-            exit(0)
-        
-        
-        self.d = d
-        self.n_heads = n_heads
-        self.d_heads = int(self.d/self.n_heads)
-        
-        self.pe = None 
-
-        self.wq = nn.Linear(self.d, self.d)
-        self.wk = nn.Linear(self.d, self.d)
-        self.wv = nn.Linear(self.d, self.d)
-
-        #initialise the blocks
-        for m in self.children():
-            init_weights(m, init_type='kaiming')
-        
-        
-    def forward(self, x):
-        bs, d, h, w = x.shape
-        self.n = w*h
-        self.w = w
-        self.h = h
-
-        if self.pe == None:
-            self.pe = positionalencoding2d(self.d, self.h, self.w).cuda()
-
-
-        # Positionnal encodding
-        x = x + self.pe.repeat(bs,1,1,1)
-
-
-        # Get queries, keys, values
-        x = torch.reshape(x, (bs, self.d, self.n))
-        x = x.permute(0,2,1)
-        Q = self.wq(x)
-        K = self.wk(x)
-        V = self.wv(x)
-
-        # Reshaping
-        Q = torch.reshape(Q, (bs,self.n,self.n_heads, self.d_heads))
-        K = torch.reshape(K, (bs,self.n,self.n_heads, self.d_heads))
-        V = torch.reshape(V, (bs,self.n,self.n_heads, self.d_heads))
-
-        # Permuting
-        Q = Q.permute((0,2,1,3))
-        K = K.permute((0,2,3,1))
-        V = V.permute((0,2,1,3))
-
-        # Self attention
-        Y = self.attention(Q,K,V)
-
-        # Inverse permute reshape
-        Y = Y.permute(0,2,1,3)
-        Y = torch.reshape(Y, (bs, self.n, self.d))
-        Y = Y.permute(0,2,1)
-        Y = torch.reshape(Y, (bs, self.d, self. h, self.w))
-        
-        return Y, Q, K, V
-
-
-    def attention(self, Q, K, V):
-        M = torch.matmul(Q,K)/(self.d**0.5)
-        A = nn.functional.softmax(M, dim = -1)
-        return torch.matmul(A,V)
 
 
 
 
-class MHCA(nn.Module):
-    def __init__(self, d):
-        self.s_pe = None
-        self.y_pe = None
-
-        self.d = d
-
-        self.wq = nn.Linear(2*self.d, 2*self.d)
-        self.wk = nn.Linear(2*self.d, 2*self.d)
-        self.wv = nn.Linear(self.d, self.d)
-
-        self.up = nn.Sequential(nn.UpsamplingBilinear2d(scale_factor=2),
-                            nn.Conv3d(2*self.d, 2*self.d, 3, 1, 1),)
-
-        self.conv1 = nn.Sequential(nn.Conv3d(2*self.d, 2*self.d, 1, 1, 1),
-                                   nn.BatchNorm3d(2*self.d),
-                                   nn.ReLU(inplace=True),)
-        self.conv2 = nn.Sequential(nn.Conv3d(self.d, self.d, 1, 1, 1),
-                                   nn.BatchNorm3d(self.d),
-                                   nn.ReLU(inplace=True),)
-        self.conv3 = nn.Sequential(nn.Conv3d(2*self.d, self.d, 1, 1, 1),
-                                   nn.BatchNorm3d(self.d),
-                                   nn.ReLU(inplace=True),)
-
-        self.sigConv = nn.Sequential(nn.Conv3d(self.d, self.d, 1, 1, 1),
-                                   nn.BatchNorm3d(2*self.d),
-                                   nn.Sigmoid(),
-                                   nn.UpsamplingBilinear2d(scale_factor=2),)
-
-    def forward(self, y, s):
-        bs, dy, hy, wy = y.shape
-        self.ny = wy*hy
-        self.wy = wy
-        self.hy = hy
-
-        _, ds, hs, ws = s.shape
-        self.ns = ws*hs
-        self.ws = ws
-        self.hs = hs
-
-        if self.pe == None:
-            self.y_pe = positionalencoding2d(self.yd, self.yh, self.yw).cuda()
-            self.s_pe = positionalencoding2d(self.sd, self.sh, self.sw).cuda()
-
-
-        # Positionnal encodding
-        y = y + self.y_pe.repeat(bs,1,1,1)
-        s = s + self.s_pe.repeat(bs,1,1,1)
-
-        # Convs and up
-        y_c1 = self.conv1(y)
-        s_c2 = self.conv2(s)
-        y_c3 = self.up(y)
-        y_c3 = self.conv3(y_c3)
-
-
-        # Get queries, keys, values
-        y_c1 = torch.reshape(y_c1, (bs, 2*self.d, self.ny))
-        y_c1 = y_c1.permute(0,2,1)
-        s_c2 = torch.reshape(s_c2, (bs, self.d, self.ns))
-        s_c2 = s_c2.permute(0,2,1)
-
-        Q = self.wq(y_c1)
-        K = self.wk(y_c1)
-        V = self.wv(s_c2)
-
-        # Reshaping
-        Q = torch.reshape(Q, (bs,self.ny,self.n_heads, self.d_heads))
-        K = torch.reshape(K, (bs,self.ny,self.n_heads, self.d_heads))
-        V = torch.reshape(V, (bs,self.ns,self.n_heads, self.d_heads))
-
-        # Permuting
-        Q = Q.permute((0,2,1,3))
-        K = K.permute((0,2,3,1))
-        V = V.permute((0,2,1,3))
-
-        # Self attention
-        Z = self.attention(Q,K,V)
-
-        # Inverse permute reshape
-        Z = Z.permute(0,2,1,3)
-        Z = torch.reshape(Z, (bs, self.n, self.d))
-        Z = Z.permute(0,2,1)
-        Z = torch.reshape(Z, (bs, self.d, self. h, self.w))
-        
-        # sigmoid module and up
-        Z = self.sigConv(Z)
-        Z = Z*s
-
-        Z = torch.cat([Z, y_c3], 1)
-
-        return Z, Q, K, V
-
-    def attention(self, Q, K, V):
-        M = torch.matmul(Q,K)/(self.d**0.5)
-        A = nn.functional.softmax(M, dim = -1)
-        return torch.matmul(A,V)
