f6b420d42df29ff5ea99446506ef6d0dd766f7d2
Myyyr <loic.themyr@gmail.com>
2021-06-24 09:56:37 +0200

    all

--- neuripsconf/ma_crosstr_v506b9.py
+++ neuripsconf/ma_crosstr_v506b9.py
@@ -23,7 +23,7 @@ def count_parameters(model):
 class ExpConfig():
     def __init__(self):
         # ID and Name
-        self.id = "506b9"
+        self.id = "506b91"
         self.experiment_name = "ma_crosstr_v{}".format(self.id)
         self.debug = False
 
@@ -48,7 +48,7 @@ class ExpConfig():
         self.clip = False
         self.patched = True
         # GPU
-        self.gpu = '1'
+        self.gpu = '0'
         os.environ["CUDA_VISIBLE_DEVICES"] = self.gpu
         # torch.backends.cudnn.benchmark = False
 
