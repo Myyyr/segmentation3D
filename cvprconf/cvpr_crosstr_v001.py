26af2b1d80b22234a9171875193eecccf1914817
Myyyr <loic.themyr@gmail.com>
2021-06-29 16:27:57 +0200

    debug

--- cvprconf/cvpr_crosstr_v001.py
+++ cvprconf/cvpr_crosstr_v001.py
@@ -68,8 +68,8 @@ class ExpConfig():
         self.n_parameters = count_parameters(self.net)
         print("N PARAMS : {}".format(self.n_parameters))
 
-        # self.model_path = './checkpoints/models/deep_crosstr.pth'
-        self.model_path = './checkpoints/models/506/modlast.pt'
+        self.model_path = './checkpoints/models/new_deep_crosstr.pth'
+        # self.model_path = './checkpoints/models/506/modlast.pt'
         
          
         
@@ -115,8 +115,8 @@ class ExpConfig():
 
         self.load_lr = False
         self.load_model()
-        self.net.reinit_decoder()
-        self.net.reinit_crostrans(dim=d_model, depth=1, heads=number_of_cross_heads, dim_head=d_model, mlp_dim=1024, dropout = 0.1)
+        # self.net.reinit_decoder()
+        # self.net.reinit_crostrans(dim=d_model, depth=1, heads=number_of_cross_heads, dim_head=d_model, mlp_dim=1024, dropout = 0.1)
         self.optimizer = optim.SGD(self.net.parameters(), lr = self.lr_rate, weight_decay=3e-5, momentum=0.99, nesterov=True)
         self.optimizer.zero_grad()
         self.validate_every_k_epochs = 10
