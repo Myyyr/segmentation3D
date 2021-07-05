16ffa50ed9c8d274e57ec6a4e25bbf5724722486
MYR <loic.themyr@gmail.com>
2021-07-05 10:46:13 +0200

    cross - debug

--- savefeat.py
+++ savefeat.py
@@ -21,46 +21,17 @@ exp.net.eval()
 
 
 # Test
-# for i, data in tqdm(enumerate(testDataLoader), total = int(len(testDataLoader))):
-#     pid, pos, inputs, labels = data
-#     pid = int(pid[0,0].item())
-
-#     inputs, labels = inputs.to(device), labels.to(device)
-#     b, c, nh, nw, nd, h, w, d = inputs.shape
-#     b, ah, aw, ad = labels.shape
-#     h, w, d = 12, 12, 3
-
-#     outputs = torch.zeros((b, 512, h*nh, w*nw, d*nd)).float().cuda()
-#     crop = []
-
-
-#     for x in range(nh):
-#         for y in range(nw):
-#             for z in range(nd):
-#                 in_pos = [torch.from_numpy(np.array((x,y,z)))[None, None, ...]]
-#                 in_pos = torch.cat(in_pos+[pos], dim=1)
-#                 out_xyz = exp.net(inputs[:,:,x,y,z,...], in_pos, True, True)
-#                 outputs[:, :, x*h:(x+1)*h, y*w:(y+1)*w, z*d:(z+1)*d] = out_xyz
-
-
-#     outputs = outputs.cpu().numpy()
-#     np.save(savepath+"test/"+str(pid)+".npy", outputs)
-
-
-print("LOAD -----> OK")
-
-# Train
-for i, data in tqdm(enumerate(trainDataLoader), total = int(len(trainDataLoader))):
+for i, data in tqdm(enumerate(testDataLoader), total = int(len(testDataLoader))):
     pid, pos, inputs, labels = data
-    print("INPUT -----> OK")
     pid = int(pid[0,0].item())
 
     inputs, labels = inputs.to(device), labels.to(device)
-    print("DEVICE -----> OK")
     b, c, nh, nw, nd, h, w, d = inputs.shape
     b, ah, aw, ad = labels.shape
+    h, w, d = 12, 12, 3
 
     outputs = torch.zeros((b, 512, h*nh, w*nw, d*nd)).float().cuda()
+    crop = []
 
 
     for x in range(nh):
@@ -71,10 +42,39 @@ for i, data in tqdm(enumerate(trainDataLoader), total = int(len(trainDataLoader)
                 out_xyz = exp.net(inputs[:,:,x,y,z,...], in_pos, True, True)
                 outputs[:, :, x*h:(x+1)*h, y*w:(y+1)*w, z*d:(z+1)*d] = out_xyz
 
-    print("FORWARD ALL -----> OK")
-    outputs = outputs.cpu().numpy()
-    print("NUMPY -----> OK")
-    np.save(savepath+"train/"+str(pid)+".npy", outputs)
-    print("SAVING -----> OK")
 
-    del outputs
+    outputs = outputs.cpu().numpy()
+    # np.save(savepath+"test/"+str(pid)+".npy", outputs)
+
+
+print("LOAD -----> OK")
+
+# # Train
+# for i, data in tqdm(enumerate(trainDataLoader), total = int(len(trainDataLoader))):
+#     pid, pos, inputs, labels = data
+#     print("INPUT -----> OK")
+#     pid = int(pid[0,0].item())
+
+#     inputs, labels = inputs.to(device), labels.to(device)
+#     print("DEVICE -----> OK")
+#     b, c, nh, nw, nd, h, w, d = inputs.shape
+#     b, ah, aw, ad = labels.shape
+
+#     outputs = torch.zeros((b, 512, h*nh, w*nw, d*nd)).float().cuda()
+
+
+#     for x in range(nh):
+#         for y in range(nw):
+#             for z in range(nd):
+#                 in_pos = [torch.from_numpy(np.array((x,y,z)))[None, None, ...]]
+#                 in_pos = torch.cat(in_pos+[pos], dim=1)
+#                 out_xyz = exp.net(inputs[:,:,x,y,z,...], in_pos, True, True)
+#                 outputs[:, :, x*h:(x+1)*h, y*w:(y+1)*w, z*d:(z+1)*d] = out_xyz
+
+#     print("FORWARD ALL -----> OK")
+#     outputs = outputs.cpu().numpy()
+#     print("NUMPY -----> OK")
+#     np.save(savepath+"train/"+str(pid)+".npy", outputs)
+#     print("SAVING -----> OK")
+
+#     del outputs
