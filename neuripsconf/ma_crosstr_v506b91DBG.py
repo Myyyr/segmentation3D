c358a8d4c5175f06082edc1dce079974f6d81a55
MYR <loic.themyr@gmail.com>
2021-07-05 17:30:14 +0200

    cross - debug

--- neuripsconf/ma_crosstr_v506b91DBG.py
+++ neuripsconf/ma_crosstr_v506b91DBG.py
@@ -4,7 +4,7 @@ import os
 from models.utils import get_scheduler
 import torch.optim as optim
 import alltrain.atlasUtils as atlasUtils
-from PatchedMultiatlasDataset_v3_debug import *
+from PatchedMultiatlasDataset_debug import *
 from torch.utils.data import DataLoader
 import torch
 import torchio as tio
